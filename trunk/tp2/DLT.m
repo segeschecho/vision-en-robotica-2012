98ea0da27ee08b166aa70830f1a91a32be22d699
seges.ar@gmail.com <seges.ar@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-05-15 23:48:16 +0000

    tp2: Se sube la version beta de la implementacion de DLT, hay un problema con como vienen los datos

---
+++ trunk/tp2/DLT.m
@@ -0,0 +1,21 @@
+function Hest =  DLT(xs1i, xs2i)
+    % se reciben las correspondencias en los parametros de la funcion
+    %Se arma la matriz A de dimension 2n x 9
+    ceros = [0 0 0];
+    n = size(xs1i, 2);
+    
+    for i = 1:n
+        
+        xi = xs1i(:, i)';
+        
+        A(2*i - 1,:) = [ceros -xs2i(3,i)*xi -xs2i(2,i)*xi];
+
+        A(2*i,:) = [-xs2i(3,i)*xi ceros -xs2i(2,i)*xi];
+    end
+    
+    size(A)
+    
+    [U, D, V] = svd(A);
+    
+    Hest = reshape(V(:, 9), 3, 3)';
+end
--- trunk/tp2/tp2.m
+++ trunk/tp2/tp2.m
@@ -13,8 +13,8 @@ function tp2(img1_file, img2_file, des1_file, des2_file, kpts1_file, kpts2_file,
     % Estimate Homography
     xs1i = [xs1 ones(size(xs1,1),1)];
     xs2i = [xs2 ones(size(xs2,1),1)];
-    %Hest = DLT(xs1i', xs2i');
-    Hest = RANSAC(xs1i', xs2i');
+    Hest = DLT(xs1i', xs2i');
+    %Hest = RANSAC(xs1i', xs2i');
     
     % Show results
     showresults(H,Hest,xs1,xs2,img1_file, img2_file);
