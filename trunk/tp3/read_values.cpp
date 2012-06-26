31f8972d11fd598df2a3c4c42ad119b6c1d1418d
taihup@gmail.com <taihup@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-06-26 18:41:37 +0000

    read_value.cpp listo

--- trunk/tp3/read_values.cpp
+++ trunk/tp3/read_values.cpp
@@ -1,34 +1,50 @@
+#include <iostream>
 #include <vector>
 #include <fstream>
 #include <opencv/cv.h>
-#include <iostream>
-
 using namespace std;
 using namespace cv;
 
+void showMatrix(Mat M){
+  cout << endl << endl;
+  for ( int row = 0; row < M.rows ; ++ row ){
+    for ( int col = 0; col < M.cols; ++ col ){
+      cout << M.at<double>(row,col) << " ";
+    }
+    cout << endl << endl;
+  }
+
+  return;
+}
+
+
 int main(int argc, char *argv[])
 {
-    FileStorage fs("parameters.xml", FileStorage::READ);
+  FileStorage fs("parameters.xml", FileStorage::READ);
+  
+  cout << "se abre el archivo xml" << endl << endl;
 
-    cout << "se abre el archivo xml" << endl << endl;
+  if (!fs.isOpened()){
+    cout << "no se pude abrir el archivo" << endl;
+  }
+  
+  //cout << " valores: \n" << (string)fs.getFirstTopLevelNode() << endl;
+  
+  Mat p1, p2, dist1, dist2, r, t;
+  fs["P1"] >> p1;
+  fs["P2"] >> p2;
+  fs["dist1"] >> dist1;
+  fs["dist2"] >> dist2;
+  fs["R"] >> r;
+  fs["T"] >> t;
+  showMatrix(p1);
+  showMatrix(p2);
+  showMatrix(dist1);
+  showMatrix(dist2);
+  showMatrix(r);
+  showMatrix(t);
 
-    if (!fs.isOpened()){
-      cout << "no se pude abrir el archivo" << endl;
-    }
-
-    //cout << " valores: \n" << (string)fs.getFirstTopLevelNode() << endl;
-
-    Mat cameraMatrix2, distCoeffs2;
-
-    //cout << " valores: \n" << (string)fs["P1"] << endl;
-    
-    //
-    //FileNode P1 = fs["P2"] ;
-    fs["P1"] >> cameraMatrix2;
-    //fs[""] >> distCoeffs2;
-
-    cout << "camera matrix: " << cameraMatrix2 << endl;
-    
-    fs.release();
-    return 0;
+  fs.release();
+  return 0;
 }
+
