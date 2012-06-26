d9753db359ec29c633ca99a5e5ab89ca0654b994
seges.ar@gmail.com <seges.ar@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-06-26 17:54:28 +0000

    tp3: Se modifica el archivo read_values.cpp con el avance en la lectura del xml

--- tp3/read_values.cpp
+++ tp3/read_values.cpp
@@ -1,20 +1,33 @@
-#include <iostream>
 #include <vector>
 #include <fstream>
 #include <opencv/cv.h>
+#include <iostream>
+
 using namespace std;
 using namespace cv;
+
 int main(int argc, char *argv[])
 {
-    FileStorage fs("parameters640x480.xml", FileStorage::READ);
-    
+    FileStorage fs("parameters.xml", FileStorage::READ);
+
+    cout << "se abre el archivo xml" << endl << endl;
+
+    if (!fs.isOpened()){
+      cout << "no se pude abrir el archivo" << endl;
+    }
+
+    //cout << " valores: \n" << (string)fs.getFirstTopLevelNode() << endl;
+
     Mat cameraMatrix2, distCoeffs2;
-    //FileNode P1 = fs["P1"] ;
+
+    //cout << " valores: \n" << (string)fs["P1"] << endl;
+    
+    //
     //FileNode P1 = fs["P2"] ;
     fs["P1"] >> cameraMatrix2;
     //fs[""] >> distCoeffs2;
 
-    cout << "camera matrix: " << fs["P1"]->type() << endl;
+    cout << "camera matrix: " << cameraMatrix2 << endl;
     
     fs.release();
     return 0;
