cec8357f9e0aada4ea1ccb79c98c945db1acdde6
taihup@gmail.com <taihup@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-06-28 20:47:37 +0000

    tp3:read_values.cpp muestra ambas imagenes izquierda y derecha

--- trunk/tp3/read_values.cpp
+++ trunk/tp3/read_values.cpp
@@ -3,27 +3,21 @@
 #include <fstream>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-//using namespace std;
-//using namespace cv;
 
-/*
-void showMatrix(Mat M){
-  std::cout << std::endl << std::endl;
-  for ( int row = 0; row < M.rows ; ++ row ){
-    for ( int col = 0; col < M.cols; ++ col ){
-      std::cout << M.at<double>(row,col) << " ";
-    }
-    std::cout << std::endl << std::endl;
-  }
 
-  return;
-}
-*/
 
 int main(int argc, char *argv[])
 {
+  if (argc !=4){
+    std::cerr << "Arguments must be passing in this way: ./read_values /path/sequence/to/parameters_file.xml /path/sequence/to/left_image /path/sequence/to/rigth_image" << std::endl;
+    return -1;
+  }
+  std::string parameters_xml = argv[1];
+  std::string left_image = argv[2];
+  std::string rigth_image = argv[3];
+
   /*read the xml file */
-  cv::FileStorage fs("parameters.xml", cv::FileStorage::READ);
+  cv::FileStorage fs(parameters_xml, cv::FileStorage::READ);
   
   std::cout << "se abre el archivo xml" << std::endl << std::endl;
 
@@ -31,18 +25,18 @@ int main(int argc, char *argv[])
     std::cout << "no se pude abrir el archivo" << std::endl;
   }
   
-  cv::Mat k1, k2, dist1, dist2, r, t;
-  fs["P1"] >> k1;
-  fs["P2"] >> k2;
-  fs["dist1"] >> dist1;
-  fs["dist2"] >> dist2;
+  cv::Mat left_k, rigth_k, left_dist, rigth_dist, r, t;
+  fs["P1"] >> left_k;
+  fs["P2"] >> rigth_k;
+  fs["dist1"] >> left_dist;
+  fs["dist2"] >> rigth_dist;
   fs["R"] >> r;
   fs["T"] >> t;
 
-  std::cout << k1 << std::endl << std::endl;
-  std::cout << k2 << std::endl << std::endl;
-  std::cout << dist1 << std::endl << std::endl;
-  std::cout << dist2 << std::endl << std::endl;
+  std::cout << left_k << std::endl << std::endl;
+  std::cout << rigth_k << std::endl << std::endl;
+  std::cout << left_dist << std::endl << std::endl;
+  std::cout << rigth_dist << std::endl << std::endl;
   std::cout << r << std::endl << std::endl;
   std::cout << t << std::endl << std::endl;
 
@@ -51,55 +45,69 @@ int main(int argc, char *argv[])
   
   /* Read the image to undistorted */
 
-  cv::Mat image; 
-  image = cv::imread("./izquierda/Image3.tif", 0);
+  cv::Mat mat_left_im, mat_rigth_im; 
+  mat_left_im  = cv::imread(left_image, 0);
+  mat_rigth_im  = cv::imread(rigth_image, 0);
 
   /* computes the rotation matrices for each camera that (virtually) make both camera image planes the same plane */
-
-  cv::Mat r1, r2, k1_prima, k2_prima, q;
-  cv::Rect validRoir2;
+  cv::Mat left_r, rigth_r, left_k_prima, rigth_k_prima, q;
   double alpha = 0;
-  cv::Size newImageSize = image.size();
-  cv::Rect roi1, roi2;
+  cv::Size new_left_image_size = mat_left_im.size();
+  cv::Size new_rigth_image_size = mat_rigth_im.size();
+  cv::Rect left_roi, rigth_roi;
 
-  cv::stereoRectify(k1, dist1, k2, dist2, image.size(), r, t, r1, r2, k1_prima, k2_prima, q, alpha, cv::CALIB_ZERO_DISPARITY, newImageSize, &roi1, &roi2);
+  cv::stereoRectify(left_k, left_dist, rigth_k, rigth_dist, mat_left_im.size(), r, t, left_r, rigth_r, left_k_prima, rigth_k_prima, q, alpha, cv::CALIB_ZERO_DISPARITY, new_left_image_size , &left_roi , &rigth_roi );
 
 
   /* Computes the undistortion and rectification transformation map for each camera*/
 
-  cv::Mat map11, map12, map21, map22;
-  cv::initUndistortRectifyMap(k1, dist1, r1, k1_prima, newImageSize, CV_32FC1, map11, map12);
-  cv::initUndistortRectifyMap(k2, dist2, r2, k2_prima, newImageSize, CV_32FC1, map21, map22);
+  cv::Mat left_map1, left_map2, rigth_map1, rigth_map2;
+  cv::initUndistortRectifyMap(left_k, left_dist, left_r, left_k_prima, new_left_image_size, CV_32FC1, left_map1, left_map2);
+  cv::initUndistortRectifyMap(rigth_k, rigth_dist, rigth_r, rigth_k_prima, new_rigth_image_size, CV_32FC1, rigth_map1, rigth_map2);
   
   /* Applies a generic geometrical transformation to an image */
 
-  cv::Mat undistorted_image;
-  cv::remap(image, undistorted_image, map11, map12, cv::INTER_LINEAR, cv::BORDER_CONSTANT, 0);
-  //cv::remap(image, undistorted_image, map21, map22, INTER_LINEAR, BORDER_CONSTANT, 0);
+  cv::Mat undistorted_left_im, undistorted_rigth_im ;
+  cv::remap(mat_left_im, undistorted_left_im, left_map1, left_map2, cv::INTER_LINEAR, cv::BORDER_CONSTANT, 0);
+  cv::remap(mat_rigth_im, undistorted_rigth_im, rigth_map1, rigth_map2, cv::INTER_LINEAR, cv::BORDER_CONSTANT, 0);
 
 
   /* Show images*/
 
-  if(! image.data ){                              // Check for invalid input
-    std::cout <<  "Could not open or find the image" << std::endl ;
+  if(! mat_left_im.data ){                              // Check for invalid input
+    std::cerr <<  "Could not open or find the left image" << std::endl ;
     return -1;
   }
-
-  cv::namedWindow( "Original Image", CV_WINDOW_AUTOSIZE );// Create a window for display.
-  cv::imshow( "Original Image", image);                   // Show our image inside it.
   
-  if(! undistorted_image.data ){                              // Check for invalid input
-    std::cout <<  "Could not open or find the image" << std::endl ;
+  if(! mat_rigth_im.data ){                              // Check for invalid input
+    std::cerr <<  "Could not open or find the rigth image" << std::endl ;
     return -1;
   }
 
-  cv::namedWindow( "Undistorted Image", CV_WINDOW_AUTOSIZE );// Create a window for display.
-  cv::imshow( "Undistorted Image", undistorted_image);                   // Show our image inside it.
+  cv::namedWindow( "Original Left Image", CV_WINDOW_AUTOSIZE ); // Create a window for display.
+  cv::imshow( "Original Left Image", mat_left_im);                   // Show our image inside it.
+  
+  cv::namedWindow( "Original Rigth Image", CV_WINDOW_AUTOSIZE ); // Create a window for display.
+  cv::imshow( "Original Rigth Image", mat_rigth_im);                   // Show our image inside it.
+
+  if(! undistorted_left_im.data ){                              // Check for invalid input
+    std::cerr <<  "Could not open or find the image" << std::endl ;
+    return -1;
+  }
+
+  if(! undistorted_rigth_im.data ){                              // Check for invalid input
+    std::cerr <<  "Could not open or find the image" << std::endl ;
+    return -1;
+  }
+
+  cv::namedWindow( "Undistorted Left Image", CV_WINDOW_AUTOSIZE );// Create a window for display.
+  cv::imshow( "Undistorted Left Image", undistorted_left_im);                   // Show our image inside it.
+
+  cv::namedWindow( "Undistorted Rigth Image", CV_WINDOW_AUTOSIZE );// Create a window for display.
+  cv::imshow( "Undistorted Rigth Image", undistorted_rigth_im);                   // Show our image inside it.
 
 
   cv::waitKey(0);                                          // Wait for a keystroke in the window
-
-
   
   return 0;
 }
