5c3f802caffee9b8b5e2cf9553f25bfced3cc329
taihup@gmail.com <taihup@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-06-29 15:59:23 +0000

    tp3: libreria libelas agregada y trantando de usarla

---
+++ tp3/libelas/build/CMakeFiles/elas.dir/DependInfo.cmake
@@ -0,0 +1,17 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/src/descriptor.cpp" "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/build/CMakeFiles/elas.dir/src/descriptor.cpp.o"
+  "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/src/elas.cpp" "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/build/CMakeFiles/elas.dir/src/elas.cpp.o"
+  "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/src/filter.cpp" "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/build/CMakeFiles/elas.dir/src/filter.cpp.o"
+  "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/src/matrix.cpp" "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/build/CMakeFiles/elas.dir/src/matrix.cpp.o"
+  "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/src/triangle.cpp" "/home/pablo/svn/materias/vision en robótica/tps/tp4/src/libelas/build/CMakeFiles/elas.dir/src/triangle.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
