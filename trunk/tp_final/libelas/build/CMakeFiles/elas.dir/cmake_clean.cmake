b1ee10813673a1a62fb8c7f57da25c4aa656f599
taihup@gmail.com <taihup@gmail.com@6cb191c5-a2b7-3571-8379-f3adeab41e4d>
2012-07-05 15:47:28 +0000

    tp_final: se agrega libelas para que funcione el tp de forma modular y se agrega parameters.xml que faltaba

---
+++ trunk/tp_final/libelas/build/CMakeFiles/elas.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/elas.dir/src/filter.cpp.o"
+  "CMakeFiles/elas.dir/src/elas.cpp.o"
+  "CMakeFiles/elas.dir/src/matrix.cpp.o"
+  "CMakeFiles/elas.dir/src/descriptor.cpp.o"
+  "CMakeFiles/elas.dir/src/triangle.cpp.o"
+  "libelas.pdb"
+  "libelas.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/elas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
